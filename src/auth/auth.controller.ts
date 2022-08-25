ec10d479fd439e7b0ba3a8971d4891cbfb60a525
Abel Masila <abel@x2ai.com>
2022-08-26 01:20:38 +0300

    password hashed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+
 import { UserService } from 'src/user/user.service';
 
 @Controller()
@@ -6,6 +8,12 @@ export class AuthController {
   constructor(private userService: UserService) {}
   @Post('register')
   async register(@Body() body) {
-    return this.userService.create(body);
+    const hashed = await bcrypt.hash(body.password, 12);
+    return this.userService.create({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: hashed,
+    });
   }
 }
