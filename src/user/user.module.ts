fadb637340d6977ad4865810aac5c975b86793b1
Abel Masila <abel@x2ai.com>
2022-08-26 00:52:14 +0300

    connected to db and created users table

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,10 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { User } from './models/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
 })
 export class UserModule {}
