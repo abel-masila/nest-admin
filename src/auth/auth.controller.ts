a884a8c0a2e67c2cb3c22e869825abaa2a6849d4
Abel Masila <abel@x2ai.com>
2022-08-26 01:29:44 +0300

    Added validation pipes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 
 import { UserService } from 'src/user/user.service';
+import { RegisterDto } from './models/register.dto';
 
 @Controller()
 export class AuthController {
   constructor(private userService: UserService) {}
   @Post('register')
-  async register(@Body() body) {
+  async register(@Body() body: RegisterDto) {
+    if (body.password !== body.password_confirm) {
+      throw new BadRequestException('Passwords do not match!');
+    }
     const hashed = await bcrypt.hash(body.password, 12);
+
     return this.userService.create({
       first_name: body.first_name,
       last_name: body.last_name,
