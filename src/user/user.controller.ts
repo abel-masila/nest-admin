6538a20f9152999e9bd680e35e938b6869ed1370
Abel Masila <abel@x2ai.com>
2022-08-31 01:43:55 +0300

    create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import { UserCreateDto } from './models/user-create.dto';
 import { User } from './models/user.entity';
 import { UserService } from './user.service';
 
@@ -8,5 +10,17 @@ export class UserController {
   @Get()
   async all(): Promise<User[]> {
     return this.userService.all();
+  }
+
+  @Post()
+  async create(@Body() body: UserCreateDto): Promise<User> {
+    const password = await bcrypt.hash('1234', 12);
+
+    return this.userService.create({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password,
+    });
   }
 }
